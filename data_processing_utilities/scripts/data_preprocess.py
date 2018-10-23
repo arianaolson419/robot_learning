1877b9cbe840d711b1fed727a4f99711dc51da9a
arianaolson419 <arianaolson419@gmail.com>
2018-10-22 22:17:11 -0400

    Create dictionaries for labels

--- data_processing_utilities/scripts/data_preprocess.py
+++ data_processing_utilities/scripts/data_preprocess.py
@@ -1,6 +1,7 @@
 """We are using the SAVEE dataset, which consists of utterances of actors speaking 15 sentences for 7 different emotions. We are only training for 'positive' and 'negative' emotions, so we need to relabel the data and convert it to spectrograms."""
 
 from scipy.io import wavfile
+from spectrogram_maker import SpectrogramMaker
 import numpy as np
 
 import re
@@ -18,6 +19,9 @@ def sample_info(filename):
     emotion_label = re.split("[^a-z]+", filename)[0]
     sentence_label = filename[len(emotion_label):]
     return emotion_label, sentence_label
+
+def make_labelled_batch():
+    pass
 
 if __name__ == "__main__":
     print(sample_info("ap01.wav"))
