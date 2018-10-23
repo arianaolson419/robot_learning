64288f3cdd62cfbb1a17a7395106e62c3578b95c
arianaolson419 <arianaolson419@gmail.com>
2018-10-23 14:49:40 -0400

    Use librosa to plot spectrograms

--- data_processing_utilities/scripts/spectrogram_maker.py
+++ data_processing_utilities/scripts/spectrogram_maker.py
@@ -1,4 +1,6 @@
 from scipy import signal, misc
+import librosa
+import librosa.display
 import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join, basename
@@ -9,61 +11,65 @@ from scipy.signal import spectrogram
 
 
 class SpectrogramMaker(object):
-	def __init__(self, input_path, output_path):
-		self.input_audio_paths = [join(input_path, f) for f in listdir(input_path) if isfile(join(input_path, f))]
-		self.output_path = output_path
-		self.all_spectrograms = []
+        def __init__(self, input_path, output_path):
+                self.input_audio_paths = [join(input_path, f) for f in listdir(input_path) if isfile(join(input_path, f))]
+                self.output_path = output_path
+                self.all_spectrograms = []
 
-	def make_spectrogram(self, path):
-		""" Makes a spectrogram for the given path
-		"""
-		rate, data = wavfile.read(path)
-		return spectrogram(data, rate)
+        def make_spectrogram(self, path):
+                """ Makes a spectrogram for the given path
+                """
+                rate, data = wavfile.read(path)
+                S = np.abs(librosa.stft(data))
+                return S
 
-	def make_all_spectrograms(self):
-		""" Makes a spectrogram for each of the filepaths
-		"""
-		for path in self.input_audio_paths:
-			self.all_spectrograms.append([self.make_spectrogram(path), basename(path)])
+        def make_all_spectrograms(self):
+                """ Makes a spectrogram for each of the filepaths
+                """
+                for path in self.input_audio_paths:
+                        self.all_spectrograms.append([self.make_spectrogram(path), basename(path)])
 
-	def plot_spectrogram(self, f, t, Sxx):
-		""" Plots the given spectrogram
-		"""
-		plt.pcolormesh(t, f, Sxx)
-		plt.ylabel('Frequency [Hz]')
-		plt.xlabel('Time [sec]')
-		plt.show()
-		return plt
+        def plot_spectrogram(self, f, t, Sxx):
+                """ Plots the given spectrogram
+                """
+                pt = librosa.display.specshow(librosa.amplitude_to_db(Sxx, ref=np.max), y_axis='log', x_axis='time')
+                #plt.pcolormesh(t, f, Sxx)
+                #plt.ylabel('Frequency [Hz]')
+                #plt.xlabel('Time [sec]')
+                plt.show()
+                return pt
 
-	def save_spectrograms(self):
-		""" Saves all of the spectrograms to the specified output path with the same filename as input
-		"""
-		for spectrogram in self.all_spectrograms:
-			(f, t, Sxx), basepath = spectrogram
-			output_path = join(self.output_path, basepath[:-3]) + "png"
-			plt = self.plot_spectrogram(f, t, Sxx)
-			plt.savefig(output_path, bbox_inches='tight', dpi=300, frameon='false')
+        def save_spectrograms(self):
+                """ Saves all of the spectrograms to the specified output path with the same filename as input
+                """
+                for spectrogram in self.all_spectrograms:
+                        (f, t, Sxx), basepath = spectrogram
+                        output_path = join(self.output_path, basepath[:-3]) + "png"
+                        plt = self.plot_spectrogram(f, t, Sxx)
+                        plt.savefig(output_path, bbox_inches='tight', dpi=300, frameon='false')
 
 
-	def make_and_show_dummy(self):
-		""" For testing, generates a signal, turns it into a spectrogram, and plots it
-		"""
-		fs = 10e3
-		N = 1e5
-		amp = 2 * np.sqrt(2)
-		noise_power = 0.01 * fs / 2
-		time = np.arange(N) / float(fs)
-		mod = 500*np.cos(2*np.pi*0.25*time)
-		carrier = amp * np.sin(2*np.pi*3e3*time + mod)
-		noise = np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
-		noise *= np.exp(-time/5)
-		x = carrier + noise
-		f, t, Sxx = signal.spectrogram(x, fs)
-		plt = self.plot_spectrogram(f, t, Sxx)
-		misc.imsave("test.png", plt)
+        def make_and_show_dummy(self):
+                """ For testing, generates a signal, turns it into a spectrogram, and plots it
+                """
+                fs = 10e3
+                N = 1e5
+                amp = 2 * np.sqrt(2)
+                noise_power = 0.01 * fs / 2
+                time = np.arange(N) / float(fs)
+                mod = 500*np.cos(2*np.pi*0.25*time)
+                carrier = amp * np.sin(2*np.pi*3e3*time + mod)
+                noise = np.random.normal(scale=np.sqrt(noise_power), size=time.shape)
+                noise *= np.exp(-time/5)
+                x = carrier + noise
+                f, t, Sxx = signal.spectrogram(x, fs)
+                plt = self.plot_spectrogram(f, t, Sxx)
+                misc.imsave("test.png", plt)
 
 
 if __name__ == '__main__':
-	spectrogram_maker = SpectrogramMaker("AudioData/DC", "Spectrograms/DC")
-	spectrogram_maker.make_all_spectrograms()
-	spectrogram_maker.save_spectrograms()
+        #spectrogram_maker = SpectrogramMaker("AudioData/DC", "Spectrograms/DC")
+        spectrogram_maker = SpectrogramMaker('/home/ariana/catkin_ws/src/robot_learning/AudioData/DC', '/tmp/SpecData')
+        spectrogram_maker.make_and_show_dummy()
+        #spectrogram_maker.make_all_spectrograms()
+        #spectrogram_maker.save_spectrograms()
