505e3274fbf48bcd40bfb6f5a272b77b5574f8ba
arianaolson419 <arianaolson419@gmail.com>
2018-11-02 13:30:17 -0400

    Skeleton code for robot node

---
+++ data_processing_utilities/scripts/emotion_recognition.py
@@ -0,0 +1,14 @@
+from __future__ import print, division
+
+import rospy
+import tensorflow.keras as keras
+from tensorflow.keras.models import load_model
+
+def class EmotionRecgonition(object):
+    def __init__(self):
+        rospy.init_node('emotion_recognition')
+        self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.model = load_model('emotion_cnn.h5')
+
+    def interpret_mic_input(self):
+        """If the spectrogram is viable audio, 
